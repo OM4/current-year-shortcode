28c683806deeea0acdc14c875bd0cf03e0b0f3b3
James Collins <james@om4.com.au>
2013-11-26 14:03:50 +1100

    Initial Commit
    Based on wp-content/mu-plugins/shortcodes.php r4865

---
+++ current-year-shortcode.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Plugin Name: Current Year Shortcode
+Plugin URI: http://om4.com.au/wordpress-plugins/
+Description: Displays the current year, allowing you to automate footer copyright statements. Use th is simple shortcode: [currentyear]
+Version: 1.0
+Author: OM4
+Author URI: http://om4.com.au/
+Git URI: https://github.com/OM4/current-year-shortcode
+Git Branch: release
+License: GPLv2
+*/
+
+/*
+
+   Copyright 2012-2013 OM4 (email: info@om4.com.au    web: http://om4.com.au/)
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * [currentyear] Shortcode
+ *
+ * @param      $atts
+ * @param null $content
+ *
+ * @return string
+ */
+function om4_currentyear_shortcode( $atts, $content = null ) {
+	return date( 'Y' );
+}
+add_shortcode( 'currentyear', 'om4_currentyear_shortcode' );
